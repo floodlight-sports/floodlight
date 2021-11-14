baeaebd44bf2f7c3054f038dd5694c9c98204a86
henne <henrik.biermann@rub.de>
2021-11-14 17:50:26 +0100

    test: add dfl parser test skeleton for existing files

--- tests/io/test_dfl.py
+++ tests/io/test_dfl.py
@@ -8,8 +8,7 @@ from floodlight.io.dfl.parser import read_positions
 def test_read_positions_for_all_files(data_loc: str or Path):
 
     # iterate over files in data location
-    # DEBUG
-    for file in os.listdir(data_loc)[5:]:
+    for file in os.listdir(data_loc):
 
         # perform test battery
         print("Testing File @ " + file)
@@ -18,7 +17,7 @@ def test_read_positions_for_all_files(data_loc: str or Path):
         file = os.path.join(data_loc, file)
         match_xy = read_positions(file)
 
-        # check that each segment exists for two teams
+        # check that each segment (i.e. half) exists twice (for both home and away team)
         for obs_xy in match_xy:
             assert (
                 np.sum([obs_xy.xy.shape == obs_2_xy.xy.shape for obs_2_xy in match_xy])
