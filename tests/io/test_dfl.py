eaffeeeaef097837e42ebeb74b57f34dd5e6accf
henne <henrik.biermann@rub.de>
2021-11-15 12:18:30 +0100

    test: add placeholder for data location

--- tests/io/test_dfl.py
+++ tests/io/test_dfl.py
@@ -10,17 +10,18 @@ def test_read_positions_for_all_files():
     # TODO: define local data location with all files
     data_loc: str or Path = None
 
-    # iterate over files in data location
-    for file in os.listdir(data_loc):
+    if data_loc is not None:
+        # iterate over files in data location
+        for file in os.listdir(data_loc):
 
-        # perform test battery
-        print("Testing File @ " + file)
+            # perform test battery
+            print("Testing File @ " + file)
 
-        # read positions to XY
-        file = os.path.join(data_loc, file)
-        match_xy = read_positions(file)
+            # read positions to XY
+            file = os.path.join(data_loc, file)
+            match_xy = read_positions(file)
 
-        # assert that match_xy is a List of XY objects
-        assert isinstance(match_xy, list)
-        for xy in match_xy:
-            assert isinstance(xy, XY)
+            # assert that match_xy is a List of XY objects
+            assert isinstance(match_xy, list)
+            for xy in match_xy:
+                assert isinstance(xy, XY)
