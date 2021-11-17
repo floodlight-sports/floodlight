8cbb159f58313786d099088f01ff915c108c8ca9
hbiermann95 <henrik.biermann@rub.de>
2021-11-17 11:08:24 +0100

    test: changed structure of statsperform parser test

--- tests/io/test_statsperform.py
+++ tests/io/test_statsperform.py
@@ -1,25 +1,24 @@
 import os
+from typing import Union
 from pathlib import Path
 
 from floodlight.io.statsperform import read_positions
 from floodlight.core.xy import XY
+from floodlight.core.pitch import Pitch
 
 
-def test_read_positions_for_all_files():
-    data_loc: str or Path = None
+def test_read_positions_for_all_files(data_loc: Union[str, Path] = None):
 
     if data_loc is not None:
         for file in os.listdir(str(data_loc)):
-            xy = read_positions(os.path.join(data_loc, file))
+            data = read_positions(os.path.join(data_loc, file))
 
             # perform test battery
             print("Testing File @ " + file)
 
-            # read positions to XY
-            file = os.path.join(data_loc, file)
-            match_xy = read_positions(file)
-
             # assert that match_xy is a List of XY objects
-            assert isinstance(match_xy, list)
-            for xy in match_xy:
-                assert isinstance(xy, XY)
+            for i, obj in enumerate(data):
+                if i < 6:
+                    assert isinstance(obj, XY)
+                else:
+                    assert isinstance(obj, Pitch)
