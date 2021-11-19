8f21a1424b96a9c48810c123b72775616310fc93
henne <henrik.biermann@rub.de>
2021-11-19 12:58:07 +0100

    feat: new dfl parser

--- floodlight/io/dfl.py
+++ floodlight/io/dfl.py
@@ -2,73 +2,64 @@ from pathlib import Path
 from typing import Dict, Tuple, Union
 
 from lxml import etree
-import numpy as np
 import datetime as dt
+import numpy as np
 
-from floodlight.core.xy import XY
+from floodlight.core.code import Code
 from floodlight.core.pitch import Pitch
+from floodlight.core.xy import XY
 
 
-def _read_matchinfo(
-    filepath_matchinfo: Union[str, Path]
-) -> Tuple[Dict[str, float], Pitch]:
-    """Reads match_information XML file and returns dictionary with meta information and
-    the playing Pitch.
+def _read_pitch_from_mat_info(filepath_mat_info: Union[str, Path]) -> Pitch:
+    """Reads match_information XML file and returns the playing Pitch.
 
     Parameters
     ----------
-    filepath_matchinfo: str or Path
-        Full path to metadata.xml file.
+    filepath_mat_info: str or Path
+        Path to XML File where the Match Information data in DFL format is saved
 
     Returns
     -------
-    metainfo: Dict
-        Dictionary with metainformation such as framerate.
-    pitch: floodlight.core.pitch.Pitch
+    pitch: Pitch
         Pitch object with actual pitch length and width.
     """
     # set up XML tree
-    tree = etree.parse(str(filepath_matchinfo))
+    tree = etree.parse(str(filepath_mat_info))
     root = tree.getroot()
 
-    # parse XML file and extract matchinfo
-    metadata = {}
-
+    # parse pitch
     length = root.find("MatchInformation").find("Environment").get("PitchX")
-    metadata["length"] = float(length) if length else None
-
+    length = float(length) if length else None
     width = root.find("MatchInformation").find("Environment").get("PitchY")
-    metadata["width"] = float(width) if width else None
-
-    pitch = Pitch(
-        length=metadata["length"],
-        width=metadata["width"],
-        xlim=(-metadata["length"] / 2, metadata["length"] / 2),
-        ylim=(-metadata["width"] / 2, metadata["width"] / 2),
-        unit="m",
-        boundaries="flexible",
-        sport="football",
+    width = float(width) if width else None
+    pitch = Pitch.from_template(
+        "dfl",
+        length=length,
+        width=width,
     )
 
-    return metadata, pitch
+    return pitch
 
 
-def create_links_from_matchinfo(
-    filepath_matchinfo: Union[str, Path]
+def create_links_from_mat_info(
+    filepath_mat_info: Union[str, Path]
 ) -> Tuple[Dict[str, Dict[int, int]], Dict[str, Dict[str, int]]]:
     """Creates links between player_id and column in the array from matchinfo XML file
 
     Parameters
     ----------
-    filepath_matchinfo: str or pathlib.Path
-        XML File where the Match Information data in DFL format is saved
+    filepath_mat_info: str or Path
+        Path to XML File where the Match Information data in DFL format is saved
 
     Returns
     -------
-
+    links: Dict[str, Dict[int, int]]
+        Dictionary mapping the jersey number to position in the team data array
+    id_to_jrsy: Dict[str, Dict[str, int]]
+        Dictionary mapping the DFL player id to jersey number
     """
     # set up XML tree
-    tree = etree.parse(str(filepath_matchinfo))
+    tree = etree.parse(str(filepath_mat_info))
     root = tree.getroot()
 
     # parse XML file and extract links from pID to xID for both teams
@@ -80,75 +71,82 @@ def create_links_from_matchinfo(
     for team in teams:
         if team.get("TeamId") == home:
             id_to_jrsy["Home"] = {
-                player.get("PersonId"): player.get("ShirtNumber")
+                player.get("PersonId"): int(player.get("ShirtNumber"))
                 for player in team.find("Players")
             }
         elif team.get("TeamId") == away:
             id_to_jrsy["Away"] = {
-                player.get("PersonId"): player.get("ShirtNumber")
+                player.get("PersonId"): int(player.get("ShirtNumber"))
                 for player in team.find("Players")
             }
         else:
             continue
 
     links = {
-        "Home": {int(jID): xID + 1 for xID, jID in enumerate(id_to_jrsy["Home"])},
-        "Away": {int(jID): xID + 1 for xID, jID in enumerate(id_to_jrsy["Away"])},
+        "Home": {
+            int(id_to_jrsy["Home"][pID]): xID + 1
+            for xID, pID in enumerate(id_to_jrsy["Home"])
+        },
+        "Away": {
+            int(id_to_jrsy["Away"][pID]): xID + 1
+            for xID, pID in enumerate(id_to_jrsy["Away"])
+        },
     }
 
     return links, id_to_jrsy
 
 
-def _create_periods_from_dat(filepath_dat: Union[str, Path]) -> Tuple[Dict, int]:
-    """Parses over position file and returns dictionary with segment timeaxis
+def _create_periods_from_dat(
+    filepath_dat: Union[str, Path]
+) -> Tuple[Dict[str, Tuple[int, int]], int]:
+    """Parses over position file and returns dictionary with periods
 
     Parameters
     ----------
     filepath_dat: str or Path
-        Full path to position.xml file.
-
+        Path to XML File where the Position data in DFL format is saved
 
     Returns
     -------
-    periods: Dict
-        Dictionary with timeaxis for the segment:
-        `periods[segment] = List of dt.datetime`.
+    periods: Dict[str, Tuple[int, int]
+        Dictionary with times for the segment:
+        `periods[segment] = (starttime, endtime)`
+    framerate: int
+        Temporal resolution of data in frames per second/Hertz.
     """
     periods = {}
     framerate = None
 
-    # parse the framesets for the ball to find the segment timeaxis
+    # retrieve information from ball frame sets
     for _, frame_set in etree.iterparse(filepath_dat, tag="FrameSet"):
         if frame_set.get("TeamId") == "Ball":
-            periods[frame_set.get("GameSection")] = [
-                dt.datetime.fromisoformat(frame.get("T"))
-                for frame in frame_set.iterfind("Frame")
-            ]
+            frames = frame_set.findall("Frame")
+            periods[frame_set.get("GameSection")] = (
+                int(frames[0].get("N")),
+                int(frames[-1].get("N")),
+            )
             if framerate is None:
-                framerate = int(
-                    1
-                    / (
-                        periods[frame_set.get("GameSection")][1]
-                        - periods[frame_set.get("GameSection")][0]
-                    ).total_seconds()
-                )
+                delta = dt.datetime.fromisoformat(
+                    frames[1].get("T")
+                ) - dt.datetime.fromisoformat(frames[0].get("T"))
+                framerate = int(round(1 / delta.total_seconds()))
 
     return periods, framerate
 
 
 def read_dfl_files(
     filepath_dat: Union[str, Path],
-    filepath_matchinfo: Union[str, Path],
+    filepath_mat_info: Union[str, Path],
     links: Dict[str, Dict[int, int]] = None,
     id_to_jrsy: Dict[str, Dict[int, int]] = None,
-) -> Tuple[XY, XY, XY, XY, XY, XY, Pitch]:
+) -> Tuple[XY, XY, XY, XY, XY, XY, Code, Code, Code, Code, Pitch]:
     """Read a DFL format position data and match information XML.
 
     Parameters
     ----------
     filepath_dat: str or pathlib.Path
         XML File where the Position data in DFL format is saved.
-    filepath_matchinfo: str or pathlib.Path
+    filepath_mat_info: str or pathlib.Path
         XML File where the Match Information data in DFL format is saved.
     links: Dict
         Dictionary mapping jersey number to position in the XY Object.
@@ -157,16 +155,16 @@ def read_dfl_files(
 
     Returns
     -------
-    List of floodlight.core.xy.XY Objects
+    List of XY Objects
     """
     # checks?
 
     # read metadata
-    metadata, pitch = _read_matchinfo(filepath_matchinfo)
+    pitch = _read_pitch_from_mat_info(filepath_mat_info)
 
     # create or check links
     if links is None or id_to_jrsy is None:
-        links, id_to_jrsy = create_links_from_matchinfo(filepath_matchinfo)
+        links, id_to_jrsy = create_links_from_mat_info(filepath_mat_info)
     else:
         pass
         # potential check
@@ -180,7 +178,9 @@ def read_dfl_files(
     number_of_away_players = max(links["Away"].values())
     number_of_frames = {}
     for segment in segments:
-        number_of_frames[segment] = len(periods[segment])
+        start = periods[segment][0]
+        end = periods[segment][1]
+        number_of_frames[segment] = end - start + 1
 
     # bins
     xydata = {
@@ -206,12 +206,11 @@ def read_dfl_files(
         for code in ["possession", "ballstatus"]
     }
 
-    # loop over framesets containing player & ball positions for all segments
+    # loop over frame sets containing player & ball positions for all segments
     for _, frame_set in etree.iterparse(filepath_dat, tag="FrameSet"):
 
         # ball
         if frame_set.get("TeamId") == "Ball":
-
             # (x, y) position
             segment = frame_set.get("GameSection")
             xydata["Ball"][segment][:, 0] = np.array(
@@ -220,7 +219,6 @@ def read_dfl_files(
             xydata["Ball"][segment][:, 1] = np.array(
                 [float(frame.get("Y")) for frame in frame_set.iterfind("Frame")]
             )
-
             # codes
             codes["ballstatus"][segment] = [
                 float(frame.get("BallStatus")) for frame in frame_set.iterfind("Frame")
@@ -232,6 +230,9 @@ def read_dfl_files(
 
         # teams
         else:
+            # find identity of frame set
+            frames = frame_set.findall("Frame")
+            segment = frame_set.get("GameSection")
             if frame_set.get("PersonId") in id_to_jrsy["Home"]:
                 team = "Home"
                 jrsy = id_to_jrsy[team][frame_set.get("PersonId")]
@@ -244,30 +245,19 @@ def read_dfl_files(
                 pass
                 # possible error or warning
 
-            # retrieve correct place to insert (x,y) data
-            segment = frame_set.get("GameSection")
-            times = [
-                dt.datetime.fromisoformat(frame.get("T"))
-                for frame in frame_set.iterfind("Frame")
-            ]
-            start = np.argmin(
-                np.abs([(t - times[0]).total_seconds() for t in periods[segment]])
-            )
-            end = np.argmin(
-                np.abs([(t - times[-1]).total_seconds() for t in periods[segment]])
-            )
+            # insert (x,y) data to correct place in bin
+            start = int(frames[0].get("N")) - periods[segment][0]
+            end = int(frames[-1].get("N")) - periods[segment][0] + 1
             x_col = (links[team][jrsy] - 1) * 2
             y_col = (links[team][jrsy] - 1) * 2 + 1
-
-            # insert to bin
-            xydata[team][segment][start : end + 1, x_col] = np.array(
-                [float(frame.get("X")) for frame in frame_set.iterfind("Frame")]
+            xydata[team][segment][start:end, x_col] = np.array(
+                [float(frame.get("X")) for frame in frames]
             )
-            xydata[team][segment][start : end + 1, y_col] = np.array(
-                [float(frame.get("Y")) for frame in frame_set.iterfind("Frame")]
+            xydata[team][segment][start:end, y_col] = np.array(
+                [float(frame.get("Y")) for frame in frames]
             )
 
-    # Create XY Objects
+    # create XY objects
     home_ht1 = XY(xy=xydata["Home"]["firstHalf"], framerate=framerate)
     home_ht2 = XY(xy=xydata["Home"]["secondHalf"], framerate=framerate)
     away_ht1 = XY(xy=xydata["Away"]["firstHalf"], framerate=framerate)
@@ -275,5 +265,43 @@ def read_dfl_files(
     ball_ht1 = XY(xy=xydata["Ball"]["firstHalf"], framerate=framerate)
     ball_ht2 = XY(xy=xydata["Ball"]["secondHalf"], framerate=framerate)
 
-    data_objects = (home_ht1, home_ht2, away_ht1, away_ht2, ball_ht1, ball_ht2, pitch)
+    # create Code objects
+    stat_ht1 = Code(
+        name="ballstatus",
+        code=codes["ballstatus"]["firstHalf"],
+        definitions={0: "Dead", 1: "Alive"},
+        framerate=framerate,
+    )
+    stat_ht2 = Code(
+        name="ballstatus",
+        code=codes["ballstatus"]["secondHalf"],
+        definitions={0: "Dead", 1: "Alive"},
+        framerate=framerate,
+    )
+    poss_ht1 = Code(
+        name="possession",
+        code=codes["possession"]["firstHalf"],
+        definitions={1: "Home", 2: "Away"},
+        framerate=framerate,
+    )
+    poss_ht2 = Code(
+        name="ballstatus",
+        code=codes["possession"]["secondHalf"],
+        definitions={1: "Home", 2: "Away"},
+        framerate=framerate,
+    )
+
+    data_objects = (
+        home_ht1,
+        home_ht2,
+        away_ht1,
+        away_ht2,
+        ball_ht1,
+        ball_ht2,
+        stat_ht1,
+        stat_ht2,
+        poss_ht1,
+        poss_ht2,
+        pitch,
+    )
     return data_objects
