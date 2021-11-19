53eedcf6202e288744ad3d5948003c52f670bd90
henne <henrik.biermann@rub.de>
2021-11-19 11:13:28 +0100

    feat: code integration

--- floodlight/io/dfl.py
+++ floodlight/io/dfl.py
@@ -55,7 +55,7 @@ def _read_matchinfo(
 
 def create_links_from_matchinfo(
     filepath_matchinfo: Union[str, Path]
-) -> Dict[str, Dict[int, int]]:
+) -> Tuple[Dict[str, Dict[int, int]], Dict[str, Dict[str, int]]]:
     """Creates links between player_id and column in the array from matchinfo XML file
 
     Parameters
@@ -72,27 +72,31 @@ def create_links_from_matchinfo(
     root = tree.getroot()
 
     # parse XML file and extract links from pID to xID for both teams
-    homeids = None
-    awayids = None
+    id_to_jrsy = {}
     teams = root.find("MatchInformation").find("Teams")
     home = root.find("MatchInformation").find("General").get("HomeTeamId")
     away = root.find("MatchInformation").find("General").get("AwayTeamId")
 
     for team in teams:
         if team.get("TeamId") == home:
-            homeids = [player.get("PersonId") for player in team.find("Players")]
+            id_to_jrsy["Home"] = {
+                player.get("PersonId"): player.get("ShirtNumber")
+                for player in team.find("Players")
+            }
         elif team.get("TeamId") == away:
-            awayids = [player.get("PersonId") for player in team.find("Players")]
+            id_to_jrsy["Away"] = {
+                player.get("PersonId"): player.get("ShirtNumber")
+                for player in team.find("Players")
+            }
         else:
             continue
-            # potential warning that matchinfo file is corrupted
 
     links = {
-        "Home": {pID: xID + 1 for xID, pID in enumerate(homeids)},
-        "Away": {pID: xID + 1 for xID, pID in enumerate(awayids)},
+        "Home": {int(jID): xID + 1 for xID, jID in enumerate(id_to_jrsy["Home"])},
+        "Away": {int(jID): xID + 1 for xID, jID in enumerate(id_to_jrsy["Away"])},
     }
 
-    return links
+    return links, id_to_jrsy
 
 
 def _create_periods_from_dat(filepath_dat: Union[str, Path]) -> Tuple[Dict, int]:
@@ -136,17 +140,20 @@ def read_dfl_files(
     filepath_dat: Union[str, Path],
     filepath_matchinfo: Union[str, Path],
     links: Dict[str, Dict[int, int]] = None,
+    id_to_jrsy: Dict[str, Dict[int, int]] = None,
 ) -> Tuple[XY, XY, XY, XY, XY, XY, Pitch]:
-    """Read a DFL format position data and match information XML
+    """Read a DFL format position data and match information XML.
 
     Parameters
     ----------
     filepath_dat: str or pathlib.Path
-        XML File where the Position data in DFL format is saved
+        XML File where the Position data in DFL format is saved.
     filepath_matchinfo: str or pathlib.Path
-        XML File where the Match Information data in DFL format is saved
+        XML File where the Match Information data in DFL format is saved.
     links: Dict
-        Dictionary from DFL PersonId to position in the XY Object
+        Dictionary mapping jersey number to position in the XY Object.
+    id_to_jrsy: Dict
+        Dictionary mapping DFL PersonId to jersey number.
 
     Returns
     -------
@@ -158,13 +165,13 @@ def read_dfl_files(
     metadata, pitch = _read_matchinfo(filepath_matchinfo)
 
     # create or check links
-    if links is None:
-        links = create_links_from_matchinfo(filepath_matchinfo)
+    if links is None or id_to_jrsy is None:
+        links, id_to_jrsy = create_links_from_matchinfo(filepath_matchinfo)
     else:
         pass
         # potential check
 
-    # create segments
+    # create periods
     periods, framerate = _create_periods_from_dat(filepath_dat)
     segments = list(periods.keys())
 
@@ -194,59 +201,71 @@ def read_dfl_files(
             for segment in segments
         },
     }
-    # codes = {
-    #     code: {segment: [] for segment in segments}
-    #     for code in ["possession", "ballstatus"]
-    # }
+    codes = {
+        code: {segment: [] for segment in segments}
+        for code in ["possession", "ballstatus"]
+    }
 
-    # loop over data filepath containing the positions
+    # loop over framesets containing player & ball positions for all segments
     for _, frame_set in etree.iterparse(filepath_dat, tag="FrameSet"):
 
-        # assign team and columns
+        # ball
         if frame_set.get("TeamId") == "Ball":
-            team = "Ball"
-            x_col = 0
-            y_col = 1
-        else:
-            pID = frame_set.get("PersonId")
-            if pID in links["Home"]:
-                team = "Home"
-            elif pID in links["Away"]:
-                team = "Away"
-            else:
-                team = None
-                pass
-                # possible error or warning
-            x_col = (links[team][pID] - 1) * 2
-            y_col = (links[team][pID] - 1) * 2 + 1
 
-        # fill rows of xy data according to playing times of the player
-        segment = frame_set.get("GameSection")
-        times = [
-            dt.datetime.fromisoformat(frame.get("T"))
-            for frame in frame_set.iterfind("Frame")
-        ]
-        if len(times) == number_of_frames[segment]:  # player did play through
-            xydata[team][segment][:, x_col] = np.array(
+            # (x, y) position
+            segment = frame_set.get("GameSection")
+            xydata["Ball"][segment][:, 0] = np.array(
                 [float(frame.get("X")) for frame in frame_set.iterfind("Frame")]
             )
-            xydata[team][segment][:, y_col] = np.array(
+            xydata["Ball"][segment][:, 1] = np.array(
                 [float(frame.get("Y")) for frame in frame_set.iterfind("Frame")]
             )
-        else:  # player did not play through
-            if times[0] < periods[segment][-1] and times[-1] > periods[segment][0]:
-                start = np.argmin(
-                    np.abs([(t - times[0]).total_seconds() for t in periods[segment]])
-                )
-                end = np.argmin(
-                    np.abs([(t - times[-1]).total_seconds() for t in periods[segment]])
-                )
-                xydata[team][segment][start : end + 1, x_col] = np.array(
-                    [float(frame.get("X")) for frame in frame_set.iterfind("Frame")]
-                )
-                xydata[team][segment][start : end + 1, y_col] = np.array(
-                    [float(frame.get("Y")) for frame in frame_set.iterfind("Frame")]
-                )
+
+            # codes
+            codes["ballstatus"][segment] = [
+                float(frame.get("BallStatus")) for frame in frame_set.iterfind("Frame")
+            ]
+            codes["possession"][segment] = [
+                float(frame.get("BallPossession"))
+                for frame in frame_set.iterfind("Frame")
+            ]
+
+        # teams
+        else:
+            if frame_set.get("PersonId") in id_to_jrsy["Home"]:
+                team = "Home"
+                jrsy = id_to_jrsy[team][frame_set.get("PersonId")]
+            elif frame_set.get("PersonId") in id_to_jrsy["Away"]:
+                team = "Away"
+                jrsy = id_to_jrsy[team][frame_set.get("PersonId")]
+            else:
+                team = None
+                jrsy = None
+                pass
+                # possible error or warning
+
+            # retrieve correct place to insert (x,y) data
+            segment = frame_set.get("GameSection")
+            times = [
+                dt.datetime.fromisoformat(frame.get("T"))
+                for frame in frame_set.iterfind("Frame")
+            ]
+            start = np.argmin(
+                np.abs([(t - times[0]).total_seconds() for t in periods[segment]])
+            )
+            end = np.argmin(
+                np.abs([(t - times[-1]).total_seconds() for t in periods[segment]])
+            )
+            x_col = (links[team][jrsy] - 1) * 2
+            y_col = (links[team][jrsy] - 1) * 2 + 1
+
+            # insert to bin
+            xydata[team][segment][start : end + 1, x_col] = np.array(
+                [float(frame.get("X")) for frame in frame_set.iterfind("Frame")]
+            )
+            xydata[team][segment][start : end + 1, y_col] = np.array(
+                [float(frame.get("Y")) for frame in frame_set.iterfind("Frame")]
+            )
 
     # Create XY Objects
     home_ht1 = XY(xy=xydata["Home"]["firstHalf"], framerate=framerate)
