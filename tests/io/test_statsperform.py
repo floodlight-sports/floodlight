5f69b5dc5759e68be9c6b255e97139d6b66433d7
henne <henrik.biermann@rub.de>
2021-11-19 13:35:30 +0100

    test: new code objects

--- tests/io/test_statsperform.py
+++ tests/io/test_statsperform.py
@@ -3,6 +3,9 @@ from typing import Union
 from pathlib import Path
 
 from floodlight.io.statsperform import read_positions
+from floodlight.core.code import Code
+from floodlight.core.pitch import Pitch
+from floodlight.core.xy import XY
 
 
 def test_read_positions_for_all_files(data_loc: Union[str, Path] = None):
@@ -13,3 +16,11 @@ def test_read_positions_for_all_files(data_loc: Union[str, Path] = None):
 
             # perform test battery
             assert isinstance(data, tuple)
+
+            for i, obj in enumerate(data):
+                if i < 6:
+                    assert isinstance(obj, XY)
+                elif i < 8:
+                    assert isinstance(obj, Code)
+                else:
+                    assert isinstance(obj, Pitch)
