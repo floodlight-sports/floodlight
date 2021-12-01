31a92824f2f230d07f67e5a4abd79aa1a095f44e
draabe <dominik.raabe92@gmail.com>
2021-12-01 18:19:22 +0100

    feat: add column type based getters

--- floodlight/core/events.py
+++ floodlight/core/events.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import ClassVar, Dict, List
 
 import pandas as pd
 
@@ -10,8 +11,50 @@ class Events:
     events: pd.DataFrame
     direction: str = None
 
+    # ToDo: outsource to file and complete
+    _special_columns: ClassVar[Dict[str, List[str]]] = {
+        "essential_columns": ["eID", "gameclock"],
+        "protected_columns": [
+            "pID",
+            "tID",
+            "mID",
+            "outcome",
+            "timestamp",
+            "minute",
+            "second",
+        ],
+    }
+
     def __str__(self):
         pass
 
     def __len__(self):
         pass
+
+    @property
+    def essential_columns(self):
+        essential_columns = list(
+            set(self.events.columns) & set(self._special_columns["essential_columns"])
+        )
+
+        return essential_columns
+
+    @property
+    def protected_columns(self):
+        protected_columns = list(
+            set(self.events.columns) & set(self._special_columns["protected_columns"])
+        )
+
+        return protected_columns
+
+    @property
+    def custom_columns(self):
+        custom_columns = list(
+            set(self.events.columns)
+            - (
+                set(self._special_columns["essential_columns"])
+                | set(self._special_columns["protected_columns"])
+            )
+        )
+
+        return custom_columns
