576afd7ed2088d66e9efd45afb1907554394e272
draabe <draabx@posteo.org>
2021-12-02 14:12:10 +0100

    feat: add dedicated definitions submodule

--- floodlight/core/events.py
+++ floodlight/core/events.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import ClassVar, Dict, List
 
 import pandas as pd
+
+from floodlight.core.definitions import essential_events_columns, protected_columns
 
 
 @dataclass
@@ -11,20 +12,6 @@ class Events:
     events: pd.DataFrame
     direction: str = None
 
-    # ToDo: outsource to file and complete
-    _special_columns: ClassVar[Dict[str, List[str]]] = {
-        "essential_columns": ["eID", "gameclock"],
-        "protected_columns": [
-            "pID",
-            "tID",
-            "mID",
-            "outcome",
-            "timestamp",
-            "minute",
-            "second",
-        ],
-    }
-
     def __str__(self):
         pass
 
@@ -32,29 +19,22 @@ class Events:
         pass
 
     @property
-    def essential_columns(self):
-        essential_columns = list(
-            set(self.events.columns) & set(self._special_columns["essential_columns"])
-        )
+    def essential(self):
+        essential = list(set(self.events.columns) & set(essential_events_columns))
 
-        return essential_columns
+        return essential
 
     @property
-    def protected_columns(self):
-        protected_columns = list(
-            set(self.events.columns) & set(self._special_columns["protected_columns"])
-        )
+    def protected(self):
+        protected = list(set(self.events.columns) & set(protected_columns))
 
-        return protected_columns
+        return protected
 
     @property
-    def custom_columns(self):
-        custom_columns = list(
+    def custom(self):
+        custom = list(
             set(self.events.columns)
-            - (
-                set(self._special_columns["essential_columns"])
-                | set(self._special_columns["protected_columns"])
-            )
+            - (set(essential_events_columns) | set(protected_columns))
         )
 
-        return custom_columns
+        return custom
