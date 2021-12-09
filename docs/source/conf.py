3507fbb604184a59304a4d802e7a9a6e0d67b825
draabe <draabx@posteo.org>
2021-12-09 03:11:16 +0100

    docs: update sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,40 +1,29 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+import datetime
 import os
 import sys
 
+# from floodlight import __version__
+
+# -- Path setup --------------------------------------------------------------
 sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
-
 project = "floodlight"
-copyright = "2021, Dominik Raabe"
+year = datetime.datetime.now().year
 author = "Dominik Raabe"
+copyright = f"{year}, {author}"
 
-# The full version, including alpha/beta/rc tags
-release = "v0.1.0"
+__version__ = "0.1.0"
+release = f"v{__version__}"
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
 ]
 
@@ -52,13 +41,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "display_version": True,
+    "collapse_navigation": False,
+    "navigation_depth": 2,
+    "prev_next_buttons_location": "top",
+    "style_external_links": True,
+    "style_nav_header_background": "#006666",
+}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+# html_css_files = ['css/custom.css']
