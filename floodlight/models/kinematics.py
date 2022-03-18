21b52fc10d5fcd4e8dbb0385dee25bc7327f38f5
manuba95 <manuel.bassek@gmail.com>
2022-03-18 15:18:28 +0100

    feat: cumulative distance

--- floodlight/models/kinematics.py
+++ floodlight/models/kinematics.py
@@ -37,13 +37,27 @@ class DistanceModel:
             f"length {self.length} and width {self.width}"
         )
 
-    def fit(self, xy: XY, direction: str = None):
+    def fit(self, xy: XY, difference: str = "central", direction: str = "plane"):
         """Fits a model to calculate euclidean distances to an xy object.
 
         Parameters
         ----------
         xy: XY
             Floodlight XY Data object.
+        difference: str
+            The method of differentiation. One of {'central', 'forward'}.\n
+            'central' will differentiate using the central difference method:
+
+                .. math::
+
+                    y^{\\prime}(t_{0}) = \\frac{y_{1}-y_{-1}}{t_{1} - t_{-1}}
+
+            'forward' will differentiate using the forward difference method:
+
+                .. math::
+
+                    y^{\\prime}(t_{0}) = \\frac{y_{1}-y_{0}}{t_{1} - t_{0}}
+
         direction: str
             One of {'x', 'y', 'plane'} of None.
             Indicates the direction to calculate the distance in.
@@ -52,8 +66,6 @@ class DistanceModel:
             'plane' or None will calculate distance in both directions.
 
         """
-
-        distance_euclidean = None
 
         if self.unit == "m":
             unit_factor = (1, 1)
@@ -66,8 +78,15 @@ class DistanceModel:
                 "Invalid argument: 'unit' has to be 'm', 'cm' or 'percent'!"
             )
 
-        if direction is None or direction == "plane":
-            differences_xy = np.gradient(xy.xy, axis=0)
+        if direction == "plane":
+            if difference == "central":
+                differences_xy = np.gradient(xy.xy, axis=0)
+            elif difference == "forward":
+                differences_xy = np.diff(xy.xy, axis=0, append=0)
+            else:
+                raise ValueError(
+                    "Invalid argument: 'difference' has to be 'central' or 'forward'!"
+                )
             distance_euclidean = np.hypot(
                 differences_xy[:, ::2] * unit_factor[0],
                 differences_xy[:, 1::2] * unit_factor[1],
@@ -78,16 +97,26 @@ class DistanceModel:
             distance_euclidean = np.gradient(xy.y * unit_factor[1], axis=0)
         else:
             raise ValueError(
-                "Invalid argument: 'direction' has to be 'plane', 'x', 'y' or None!"
+                "Invalid argument: 'direction' has to be 'plane', 'x', 'y'!"
             )
 
         self._distance_euclidean = distance_euclidean
         if xy.framerate is not None:
             self.framerate = xy.framerate
 
-    @property
-    def distance(self):
+    def distance_covered(self):
         distance = PlayerProperty(
-            property=self._distance_euclidean, name="distance", framerate=self.framerate
+            property=self._distance_euclidean,
+            name="distance_covered",
+            framerate=self.framerate,
         )
         return distance
+
+    def cumulative_distance_covered(self):
+        cum_dist = np.nancumsum(self._distance_euclidean, axis=0)
+        cumulative_distance = PlayerProperty(
+            property=cum_dist,
+            name="cumulative_distance_covered",
+            framerate=self.framerate,
+        )
+        return cumulative_distance
