3b394d3897ca12923446689c810f4d33f1ab98c4
manuba95 <manuel.bassek@gmail.com>
2022-03-21 15:08:18 +0100

    feat: AccelerationModel

--- floodlight/models/kinematics.py
+++ floodlight/models/kinematics.py
@@ -151,7 +151,7 @@ class VelocityModel:
 
     def __str__(self):
         return (
-            f"Floodlight VelicityModel Class with unit [{self.unit}],"
+            f"Floodlight VelocityModel Class with unit [{self.unit}],"
             f"length {self.length} and width {self.width}"
         )
 
@@ -213,12 +213,101 @@ class VelocityModel:
             )
 
         self._velocity = velocity
-        self.framerate = xy.framerate
+        self.framerate = distance_euclidean.framerate
 
     def velocity(self):
-        distance = PlayerProperty(
+        velocity = PlayerProperty(
             property=self._velocity,
             name="velocity",
             framerate=self.framerate,
         )
-        return distance
+        return velocity
+
+
+@dataclass
+class AccelerationModel:
+    """Class for calculating accelerations of players on the pitch.
+
+    Parameters
+    __________
+    pitch: Pitch
+        Pitch object of the data. Should at least include information about the data's
+        unit. If pitch.unit is 'percent', length and width of the pitch are also needed.
+
+    """
+
+    pitch: Pitch = None
+    length: Numeric = None
+    width: Numeric = None
+    unit: str = None
+
+    def __init__(self, pitch):
+        self.unit = pitch.unit
+        self.length = pitch.length
+        self.width = pitch.width
+        self.pitch = pitch
+        self._acceleration = None
+        self.framerate = None
+
+    def __str__(self):
+        return (
+            f"Floodlight AccelerationModel Class with unit [{self.unit}],"
+            f"length {self.length} and width {self.width}"
+        )
+
+    def fit(
+        self,
+        xy: XY,
+        difference: str = "central",
+        direction: str = "plane",
+    ):
+        """Fits a model to calculate euclidean distances to an xy object.
+
+        Parameters
+        ----------
+        xy: XY
+            Floodlight XY Data object.
+        difference: str
+            The method of differentiation. One of {'central', 'forward'}.\n
+            'central' will differentiate using the central difference method:
+
+                .. math::
+
+                    y^{\\prime}(t_{0}) = \\frac{y_{1}-y_{-1}}{t_{1} - t_{-1}}
+
+            'forward' will differentiate using the forward difference method and append
+            a '0' at the end of the array along axis 1:
+
+                .. math::
+
+                    y^{\\prime}(t_{0}) = \\frac{y_{1}-y_{0}}{t_{1} - t_{0}}
+
+        direction: str
+            One of {'x', 'y', 'plane'}.
+            Indicates the direction to calculate the distance in.
+            'x' will only calculate distance in the direction of the x-coordinate.
+            'y' will only calculate distance in the direction of the y-coordinate.
+            'plane' or None will calculate distance in both directions.
+        """
+
+        velocity_model = VelocityModel(self.pitch)
+        velocity_model.fit(xy, difference=difference, direction=direction)
+        velocity = velocity_model.velocity()
+
+        if difference == "central":
+            acceleration = np.gradient(velocity.property, axis=0) * velocity.framerate
+        else:
+            acceleration = (
+                np.diff(velocity.property, axis=0, append=0) * velocity.framerate
+            )
+
+        self._acceleration = acceleration
+        self.framerate = velocity.framerate
+
+    def acceleration(self):
+        acceleration = PlayerProperty(
+            property=self._acceleration,
+            name="acceleration",
+            framerate=self.framerate,
+        )
+        return acceleration
