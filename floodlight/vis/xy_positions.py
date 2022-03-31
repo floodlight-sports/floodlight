95ff961597b6caa4ebd2503b54a5ff57e05ebc10
Martin Wohlan <martin.wohlan@gmail.com>
2022-03-31 09:20:17 +0200

    feat: first draft plot_trajectories() method

--- floodlight/vis/xy_positions.py
+++ floodlight/vis/xy_positions.py
@@ -1,5 +1,9 @@
+from typing import Tuple
+
 import matplotlib
 import numpy as np
+
+from floodlight.utils.types import Numeric
 
 
 def plot_positions(
@@ -19,7 +23,10 @@ def plot_positions(
             If set to False marker="o". Else marker="."
         ax: matplotlib.axes
             Axes from matplotlib library on which the positions are plotted.
-
+        kwargs:
+            Optional keyworded arguments e.g. {'color', 'zorder', 'marker'}
+            which can be used for the plot functions from matplotlib. The kwargs are
+            only passed to all the plot functions of matplotlib.
         Returns
         -------
         matplotib.axes
@@ -29,7 +36,7 @@ def plot_positions(
     # kwargs which are used to configure the plot with default values.
     # All other kwargs are just getting passed to the scatter() method.
     marker = kwargs.pop("marker", "o" if not ball else ".")
-    color = kwargs.pop("color", "black")
+    color = kwargs.pop("color", "black" if not ball else "grey")
     zorder = kwargs.pop("zorder", 1)
 
     # plotting the positions
@@ -53,5 +60,53 @@ def plot_positions(
             zorder=zorder,
             **kwargs,
         )
+
+    return ax
+
+
+def plot_trajectories(
+    xy: np.ndarray,
+    frame_range: Tuple[Numeric, Numeric],
+    ball: bool,
+    ax: matplotlib.axes,
+    **kwargs,
+) -> matplotlib.axes:
+    """
+        Plots positions for a given frame on a matplotlib.axes.
+    plot
+        Parameters
+        -----------
+        xy: np.ndarray
+            Full data array containing x- and y-coordinates, where each player's
+            coordinates occupy two consecutive columns.
+        frame_range: Tuple[Numeric, Numeric]
+            Frame range for which trajectories are plotted. From frame_range[0] to
+            frame_range[1] a trajectory is plotted.
+        ball: bool
+            If set to False marker="o". Else marker="."
+        ax: matplotlib.axes
+            Axes from matplotlib library on which the positions are plotted.
+        kwargs:
+            Optional keyworded arguments e.g.{'linewidth', 'zorder', 'linestyle',
+            'alpha'} which can be used for the plot functions from matplotlib.
+            The kwargs are only passed to all the plot functions of matplotlib.
+
+        Returns
+        -------
+        matplotib.axes
+            A matplotlib.axes on which x and y-positions of a given frame are plotted.
+    """
+
+    # kwargs which are used to configure the plot with default values.
+    # All other kwargs are just getting passed to the plot() method.
+    color = kwargs.pop("color", "black")
+    zorder = kwargs.pop("zorder", 1)
+    # ball trajectories are thinner per default
+    linewidth = kwargs.pop("linewidth", 1 if not ball else 0.5)
+
+    for i in range(0, len(xy[0]), 2):
+        x = xy[frame_range[0] : frame_range[1], i]
+        y = xy[frame_range[0] : frame_range[1], i + 1]
+        ax.plot(x, y, color=color, zorder=zorder, linewidth=linewidth, **kwargs)
 
     return ax
