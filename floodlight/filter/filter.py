6f882969cd5ea43e29c7fa876310df56a524a9fa
manuba95 <manuel.bassek@gmail.com>
2022-04-13 15:31:52 +0200

    feat: butterworth lowpass filter

--- floodlight/filter/filter.py
+++ floodlight/filter/filter.py
@@ -1,11 +1,13 @@
-from typing import List, Dict
+from typing import Dict
+from floodlight.utils.types import Numeric
 
 import numpy as np
+import scipy.signal
 
 from floodlight import XY
 
 
-def get_sequences_without_nans(data_column: np.ndarray) -> np.ndarray:
+def _get_sequences_without_nans(data_column: np.ndarray) -> np.ndarray:
     """Gets indices of sequences without NaNs from a sequence of data.
 
     Parameters
@@ -42,10 +44,53 @@ def get_sequences_without_nans(data_column: np.ndarray) -> np.ndarray:
     return sequences
 
 
-def get_sequences_from_xy(
-    xy: XY, team_links: Dict[str, int]
-) -> List[Dict[str, np.ndarray]]:
-    """Gets sequences without NaNs from a xy-Objects.
+def _filter_sequence_butterworth_lowpass(
+    signal: np.ndarray, order: int = 3, cutoff: Numeric = 1, framerate: Numeric = None
+) -> np.ndarray:
+    """Filters a signal with a analog Butterworth low pass filter.
+
+    Parameters
+    ----------
+    signal: np.ndarray
+        Sequence of coordinates in a xy-Object.
+    order: int
+        The order of the filter.
+    cutoff: Numeric
+        The critical frequency. If ``framerate`` is not specified, the cutoff is
+        normalized from 0 to 1, where 1 is the Nyquist frequency.
+    framerate: Numeric, optional
+        The sampling frequency of the data.
+
+    Returns
+    -------
+    filt_signal: np.array
+        Signal filtered by the Butterworth filter
+    """
+
+    coeffs = scipy.signal.butter(
+        order,
+        cutoff,
+        btype="lowpass",
+        output="ba",
+        fs=framerate,
+    )
+
+    filt_signal = scipy.signal.filtfilt(
+        coeffs[0], coeffs[1], signal, method="pad", axis=0
+    )
+
+    return filt_signal
+
+
+def filter_xy_butterworth_lowpass(
+    xy: XY,
+    team_links: Dict[str, int],
+    order: int = 3,
+    cutoff: Numeric = 1,
+    remove_short_seqs: bool = True,
+) -> XY:
+    """Filters the position data inside a XY-object with a digital Butterworth lowpass
+    filter.
 
     Parameters
     ----------
@@ -53,22 +98,56 @@ def get_sequences_from_xy(
         List of XY-objects from any floodlight parser.
     team_links: Dict[str, int]
         Link-dictionary of the form ``links[identifier-ID] = xID``.
+    order: int
+        The order of the filter.
+    cutoff: Numeric
+        The critical frequency. If ``framerate`` is not specified, the cutoff is
+        normalized from 0 to 1, where 1 is the Nyquist frequency.
+    remove_short_seqs: bool
+        If True, sequences that are to short for the Filter with the specified settings
+        are removed from the data. If False, they are kept unfiltered
+
+
     Returns
     -------
-    sequence_list: Dict[str, np.ndarray]
-        List with dictionaries for each player with key identifier-ID and value nd.array
-        of dimensions n x 2. First entry corresponds to the first frame in a sequence
-        without NaNs, second entry corresponds to the last frame (excluded) in a
-        sequence without NaNs.
+    xy_filtered: XY
+        XY object with position data filtered by designed Butterworth low pass filter.
     """
 
-    nan_dict = {}
+    min_signal_len = 3 * (order + 1)
+    framerate = xy.framerate
+
+    xy_filt = np.empty(xy.xy.shape)
     for player in team_links:
         # players x-index in xy-Object
         pos_idx = team_links[player] * 2
+        pos_idy = team_links[player] * 2 + 1
 
-        data_col = xy[:, pos_idx]
-        sequences = get_sequences_without_nans(data_col)
-        nan_dict.update({player: sequences})
+        data_col_x = xy[:, pos_idx]
+        data_col_y = xy[:, pos_idy]
 
-    return nan_dict
+        sequences = _get_sequences_without_nans(data_col_x)
+
+        col_filt_x = np.full(data_col_x.shape, np.nan)
+        col_filt_y = np.full(data_col_y.shape, np.nan)
+
+        for seq in sequences:
+            if np.diff(seq) > min_signal_len:
+                col_filt_x[seq[0] : seq[1]] = _filter_sequence_butterworth_lowpass(
+                    data_col_x[seq[0] : seq[1]], order, cutoff, framerate
+                )
+                col_filt_y[seq[0] : seq[1]] = _filter_sequence_butterworth_lowpass(
+                    data_col_y[seq[0] : seq[1]], order, cutoff, framerate
+                )
+            elif remove_short_seqs is False:
+                col_filt_x[seq[0] : seq[1]] = data_col_x[seq[0], seq[1]]
+                col_filt_y[seq[0] : seq[1]] = data_col_y[seq[0], seq[1]]
+            else:
+                pass
+
+        xy_filt[:, pos_idx] = col_filt_x
+        xy_filt[:, pos_idy] = col_filt_y
+
+    xy_filtered = XY(xy=xy_filt, framerate=xy.framerate, direction=xy.direction)
+
+    return xy_filtered
