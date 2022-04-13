a90bf364692c0c4400f71af2dd1fab1b9d61da66
manuba95 <manuel.bassek@gmail.com>
2022-04-13 18:56:27 +0200

    feat: Savitzky-Golay lowpass filter

--- floodlight/filter/filter.py
+++ floodlight/filter/filter.py
@@ -151,3 +151,73 @@ def filter_xy_butterworth_lowpass(
     xy_filtered = XY(xy=xy_filt, framerate=xy.framerate, direction=xy.direction)
 
     return xy_filtered
+
+
+def filter_xy_savgol_lowpass(
+    xy: XY,
+    team_links: Dict[str, int],
+    window_length: int = 5,
+    poly_order: Numeric = 3,
+    remove_short_seqs: bool = True,
+) -> XY:
+    """Filters the position data inside a XY-object with a Savitzky-Golay filter.
+    filter.
+
+    Parameters
+    ----------
+    xy: XY
+        List of XY-objects from any floodlight parser.
+    team_links: Dict[str, int]
+        Link-dictionary of the form ``links[identifier-ID] = xID``.
+    window_length: int
+        The length of the filter window.
+    poly_order: Numeric
+        The order of the polynomial used to fit the samples. ``poly_order`` must be less
+        than ``window_length``
+    remove_short_seqs: bool
+        If True, sequences that are to short for the Filter with the specified settings
+        are removed from the data. If False, they are kept unfiltered
+
+
+    Returns
+    -------
+    xy_filtered: XY
+        XY object with position data filtered by designed Butterworth low pass filter.
+    """
+
+    min_signal_len = window_length
+
+    xy_filt = np.empty(xy.xy.shape)
+    for player in team_links:
+        # players x-index in xy-Object
+        pos_idx = team_links[player] * 2
+        pos_idy = team_links[player] * 2 + 1
+
+        data_col_x = xy[:, pos_idx]
+        data_col_y = xy[:, pos_idy]
+
+        sequences = _get_sequences_without_nans(data_col_x)
+
+        col_filt_x = np.full(data_col_x.shape, np.nan)
+        col_filt_y = np.full(data_col_y.shape, np.nan)
+
+        for seq in sequences:
+            if np.diff(seq) > min_signal_len:
+                col_filt_x[seq[0] : seq[1]] = scipy.signal.savgol_filter(
+                    data_col_x[seq[0] : seq[1]], window_length, poly_order
+                )
+                col_filt_y[seq[0] : seq[1]] = scipy.signal.savgol_filter(
+                    data_col_y[seq[0] : seq[1]], window_length, poly_order
+                )
+            elif remove_short_seqs is False:
+                col_filt_x[seq[0] : seq[1]] = data_col_x[seq[0], seq[1]]
+                col_filt_y[seq[0] : seq[1]] = data_col_y[seq[0], seq[1]]
+            else:
+                pass
+
+        xy_filt[:, pos_idx] = col_filt_x
+        xy_filt[:, pos_idy] = col_filt_y
+
+    xy_filtered = XY(xy=xy_filt, framerate=xy.framerate, direction=xy.direction)
+
+    return xy_filtered
