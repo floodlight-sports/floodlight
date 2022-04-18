a3166e1b3b9a57cf2f2c0ede4fae1003a1b5a97f
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2022-04-18 14:02:09 +0200

    !load_eigd loads data
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- floodlight/io/eigd.py
+++ floodlight/io/eigd.py
@@ -3,11 +3,15 @@ import tempfile
 import urllib.request
 from typing import List
 
+import h5py
+
 from settings import EIGD_HOST_URL, ROOT_DIR
 
 
-def load_eigd() -> List:
-    return ["No", "Yes"]
+def load_eigd(file_pos_h5=f'{ROOT_DIR}/tmp/48dcd3_00-06-00.h5') -> List:
+    with h5py.File(file_pos_h5) as h5f:
+        pos_dict = {pos_set: positions[()] for pos_set, positions in h5f.items()}
+    return pos_dict
 
 
 def _download():
