3db031a33c0d0eb11e431312caebc424818343d5
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2022-04-19 13:25:17 +0200

    !data eigd class
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- floodlight/io/eigd.py
+++ floodlight/io/eigd.py
@@ -1,30 +1,43 @@
-import shutil
+import os
 import tempfile
-import urllib.request
 from typing import List
 
 import h5py
 
-from settings import EIGD_HOST_URL, ROOT_DIR
+from floodlight.utils.extract import extract_zip
+from floodlight.utils.loader import down_loader
+from settings import EIGD_HOST_URL, ROOT_DIR, DATA_DIR
 
 
 def load_eigd(file_pos_h5=f'{ROOT_DIR}/tmp/48dcd3_00-06-00.h5') -> List:
-    _unpack()
     with h5py.File(file_pos_h5) as h5f:
         pos_dict = {pos_set: positions[()] for pos_set, positions in h5f.items()}
     return pos_dict
 
 
-def _download():
-    response = urllib.request.urlopen(EIGD_HOST_URL)
-    return response.status, response.read()
+class Eigd_Iterator:
+    def __init__(self):
+        self._index = 0
+
+    def __next__(self):
+        pass
 
 
-def _unpack():
-    status, file = _download()
+class Dataset_Eigd:
+    def __init__(self, dataset_path='eigd_dataset'):
+        data_dir = os.path.isdir(os.path.join(DATA_DIR, dataset_path))
+        if not data_dir or bool(os.listdir(data_dir)):
+            self._download_and_extract()
 
-    tmp = tempfile.NamedTemporaryFile()
-    tmp.write(file)
+    def __iter__(self):
+        return Eigd_Iterator(self)
 
-    shutil.unpack_archive(tmp.name, f'{ROOT_DIR}/tmp', format='zip')
-    tmp.close()
+    def get_dataset(self):
+        pass
+
+    @staticmethod
+    def _download_and_extract():
+        tmp = tempfile.NamedTemporaryFile()
+        tmp.write(down_loader(EIGD_HOST_URL))
+        extract_zip(tmp.name, DATA_DIR)
+        tmp.close()
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, '.data')
 
 # eigd server information
 EIGD_SCHEMA = 'https'
