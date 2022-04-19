33a31f49c9a2cef7ccc9ffafde2cb80017f37584
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2022-04-19 21:51:23 +0200

    !fixing more
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- settings.py
+++ settings.py
@@ -8,3 +8,4 @@ EIGD_SCHEMA = 'https'
 EIGD_BASE_URL = "data.uni-hannover.de/dataset/8ccb364e-145f-4b28-8ff4-954b86e9b30d/resource/fd24e032-742d-4609-9052-cec310a2a563/download"
 EIGD_FILENAME = "eigd-h_pos.zip"
 EIGD_HOST_URL = f'{EIGD_SCHEMA}://{EIGD_BASE_URL}/{EIGD_FILENAME}'
+EIGD_FRAMERATE = 20
