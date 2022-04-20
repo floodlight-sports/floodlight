3397634c41d56f6bc15af5518bd85feb7c37bbd0
robertreingit <git@robertrein.de>
2022-04-20 08:56:39 +0200

    feat: add approximaty entropy metric.

---
+++ floodlight/metrics/apen.py
@@ -0,0 +1,47 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def approx_entropy(sig: npt.NDArray, m: int = 2, r: float = 0.5) -> float:
+    """Calculates the Approximate Entropy ApEn(m,r) of sig according to Pincus (1991).
+
+    Constraints:
+        Time-series must be taken at equally spaced time points.
+        Lower bound according to Pincus, Gladstone, Ehrenkranz (1991) is 50 time points.
+        The filtering level r should be at least three times larger in magnitude
+        as the noise. Rule of thumb: 0.1-0.25 of data STD.
+
+    References:
+        Pincus, S. M. (1991). Approximate entropy as a measure of system complexity.
+            Proceedings of the National Academy of Sciences, 88(6), 2297-2301.
+        Pincus, S. M., Gladstone, I. M., & Ehrenkranz, R. A. (1991). A regularity
+            statistic for medical data analysis. Journal of clinical monitoring, 7(4),
+            335-345.
+
+    Parameters
+    ----------
+    sig: time-series as numpy array
+    m: comparison length of runs (integer), typical m in {2,3}
+    r: filtering level (real number)
+
+    Returns
+    -------
+    ApEn: float - the Approximate Entropy of sig
+    """
+    N = len(sig)
+
+    def phi_m(m_):
+        no_parts = N - m_ + 1
+        x_i_s = np.zeros((no_parts, m_))
+        for i in range(no_parts):
+            x_i_s[i, :] = sig[i:(i + m_)]
+        c_i_m_r_s = np.zeros(no_parts)
+        for i in range(no_parts):
+            d_i_j = np.max(np.abs(x_i_s - x_i_s[i, :]), axis=1)
+            c_i_m_r_s[i] = np.sum(d_i_j <= r)
+        return np.sum(np.log(c_i_m_r_s)) / no_parts - np.log(no_parts)
+
+    ap = phi_m(m) - phi_m(m + 1)
+    if ap < np.finfo('float64').eps:
+        ap = 0.0
+    return ap
---
+++ tests/test_metrics/test_apen.py
@@ -0,0 +1,46 @@
+import pytest
+import numpy as np
+from floodlight.metrics.apen import approx_entropy
+
+
+@pytest.mark.unit
+def test_logistic_map(check_vals=[0, 0.23, 0.45], precision=0.1) -> None:
+    """Check results for logistic map according to Pincus (1991)
+    """
+
+    def logistic_map(N, R, x_0=np.random.uniform(0.0, 1.0)):
+        x = np.zeros(N)
+        x[0] = x_0
+        for i in range(N - 1):
+            x[i + 1] = R * x[i] * (1 - x[i])
+        return x
+
+    R = [3.5, 3.6, 3.8]
+    N = [300, 1000, 3000]
+    apens_logistic = np.zeros(len(R) * len(N))
+    counter = 0
+    for r in R:
+        log_map = logistic_map(np.max(N), r, x_0=0.5)
+        for n in N:
+            apens_logistic[counter] = approx_entropy(log_map[0:n], 2, 0.025)
+            counter += 1
+    apens_logistic.shape = (3, 3)
+    assert np.all(np.abs(np.mean(apens_logistic, axis=1) - check_vals) < precision)
+
+
+@pytest.mark.unit
+def test_series_5(precision=0.1, reps=100) -> None:
+    """Check results according to Pincus et al. (1991)
+    """
+    signal = np.tile([1, 2, 1, 3], reps)
+    chk_01 = np.abs(approx_entropy(signal, m=1, r=0.5) - 0.5 * np.log(2.0)) < precision
+    chk_02 = np.abs(approx_entropy(signal, m=2, r=0.5)) < precision
+    assert chk_01 and chk_02
+
+
+@pytest.mark.unit
+def test_series_7(precision=0.01, N=400) -> None:
+    """Check results according to Pincus et al. (1991)
+    """
+    signal = np.random.choice([0.002, -0.001], N) + np.tile([1, 0], int(N / 2))
+    assert np.abs(approx_entropy(signal, m=2, r=0.5)) < precision
