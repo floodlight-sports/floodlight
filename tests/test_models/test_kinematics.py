57e81524aab7bf04929d6dca230acb0f3d74c749
manuba95 <manuel.bassek@gmail.com>
2022-04-22 11:55:07 +0200

    refactor: test structure

--- tests/test_models/test_kinematics.py
+++ tests/test_models/test_kinematics.py
@@ -23,60 +23,6 @@ def test_distance_covered_difference_central(example_xy_object_kinematics):
 
 
 @pytest.mark.unit
-def test_cumulative_distance_covered_difference_central(example_xy_object_kinematics):
-    # Arrange
-    xy = example_xy_object_kinematics
-
-    # Act
-    dist_model = DistanceModel()
-    dist_model.fit(xy)
-    distance_covered = dist_model.cumulative_distance_covered()
-
-    # Assert
-    assert np.array_equal(
-        np.round(distance_covered, 3),
-        np.array(((1, 0), (2.118, 1.414), (3.532, 1.414))),
-        equal_nan=True,
-    )
-
-
-@pytest.mark.unit
-def test_velocity_difference_central(example_xy_object_kinematics):
-    # Arrange
-    xy = example_xy_object_kinematics
-
-    # Act
-    vel_model = VelocityModel()
-    vel_model.fit(xy)
-    velocity = vel_model.velocity()
-
-    # Assert
-    assert np.array_equal(
-        np.round(velocity, 3),
-        np.array(((20, np.NaN), (22.361, 28.284), (28.284, np.NaN))),
-        equal_nan=True,
-    )
-
-
-@pytest.mark.unit
-def test_acceleration_difference_central(example_xy_object_kinematics):
-    # Arrange
-    xy = example_xy_object_kinematics
-
-    # Act
-    acc_model = AccelerationModel()
-    acc_model.fit(xy)
-    acceleration = acc_model.acceleration()
-
-    # Assert
-    assert np.array_equal(
-        np.round(acceleration, 3),
-        np.array(((47.214, np.NaN), (82.843, np.NaN), (118.472, np.NaN))),
-        equal_nan=True,
-    )
-
-
-@pytest.mark.unit
 def test_distance_covered_difference_backward(example_xy_object_kinematics):
     # Arrange
     xy = example_xy_object_kinematics
@@ -90,6 +36,24 @@ def test_distance_covered_difference_backward(example_xy_object_kinematics):
     assert np.array_equal(
         np.round(distance_covered, 3),
         np.array(((0, 1.414), (1, np.NaN), (1.414, np.NaN))),
+        equal_nan=True,
+    )
+
+
+@pytest.mark.unit
+def test_cumulative_distance_covered_difference_central(example_xy_object_kinematics):
+    # Arrange
+    xy = example_xy_object_kinematics
+
+    # Act
+    dist_model = DistanceModel()
+    dist_model.fit(xy)
+    distance_covered = dist_model.cumulative_distance_covered()
+
+    # Assert
+    assert np.array_equal(
+        np.round(distance_covered, 3),
+        np.array(((1, 0), (2.118, 1.414), (3.532, 1.414))),
         equal_nan=True,
     )
 
@@ -113,6 +77,24 @@ def test_cumulative_distance_covered_difference_backward(example_xy_object_kinem
 
 
 @pytest.mark.unit
+def test_velocity_difference_central(example_xy_object_kinematics):
+    # Arrange
+    xy = example_xy_object_kinematics
+
+    # Act
+    vel_model = VelocityModel()
+    vel_model.fit(xy)
+    velocity = vel_model.velocity()
+
+    # Assert
+    assert np.array_equal(
+        np.round(velocity, 3),
+        np.array(((20, np.NaN), (22.361, 28.284), (28.284, np.NaN))),
+        equal_nan=True,
+    )
+
+
+@pytest.mark.unit
 def test_velocity_difference_backward(example_xy_object_kinematics):
     # Arrange
     xy = example_xy_object_kinematics
@@ -126,6 +108,24 @@ def test_velocity_difference_backward(example_xy_object_kinematics):
     assert np.array_equal(
         np.round(velocity, 3),
         np.array(((0, 28.284), (20, np.NaN), (28.284, np.NaN))),
+        equal_nan=True,
+    )
+
+
+@pytest.mark.unit
+def test_acceleration_difference_central(example_xy_object_kinematics):
+    # Arrange
+    xy = example_xy_object_kinematics
+
+    # Act
+    acc_model = AccelerationModel()
+    acc_model.fit(xy)
+    acceleration = acc_model.acceleration()
+
+    # Assert
+    assert np.array_equal(
+        np.round(acceleration, 3),
+        np.array(((47.214, np.NaN), (82.843, np.NaN), (118.472, np.NaN))),
         equal_nan=True,
     )
 
