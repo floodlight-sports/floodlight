cd389c7677eb886e336e54f03ee0e7b00cc3ce73
Martin Wohlan <martin.wohlan@gmail.com>
2022-04-28 14:23:36 +0200

    test: XY object fixture

--- tests/test_vis/conftest.py
+++ tests/test_vis/conftest.py
@@ -1,5 +1,8 @@
-import pytest
 import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+from floodlight.core.xy import XY
 
 
 @pytest.fixture()
@@ -32,3 +35,17 @@ def example_input_plot_handball_pitch_axis_ticks() -> []:
     input = [(0, 40), (0, 20), "m", "standard", True, ax]
 
     return input
+
+
+@pytest.fixture()
+def example_xy_object() -> XY:
+    pos = np.array(
+        [
+            [35, 5, 35, 63, 25, 25, 25, 50],
+            [45, 10, 45, 55, 35, 20, 35, 45],
+            [55, 10, 55, 55, 45, 20, 45, 45],
+            [88.5, 20, 88.5, 30, 88.5, 40, 88.5, 50],
+        ]
+    )
+
+    return XY(pos)
