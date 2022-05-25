cdce2cdb3013177aa7b93b71d01ee6659e0548b5
draabe <draabx@posteo.org>
2022-05-25 18:07:19 +0200

    fix: broken settings path

--- floodlight/settings.py
+++ floodlight/settings.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(ROOT_DIR, ".data")
+ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent
+DATA_DIR = ROOT_DIR / ".data"
