215eb4f7b28111b9baf9d585acd4fc52ccdec16e
manuba95 <manuel.bassek@gmail.com>
2023-01-11 14:48:01 +0100

    docs: docstrings, inline comments and typing

--- floodlight/io/sportradar.py
+++ floodlight/io/sportradar.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Dict, Union
+
 import datetime
 import json
 import pandas as pd
@@ -5,41 +8,81 @@ import pandas as pd
 from floodlight import Events
 
 
-def read_sportradar_timeline(filepath_events):
-    """
-    Reads the sport radar timeline json files.
+def read_sportradar_timeline(
+    filepath_events: Union[str, Path]
+) -> Dict[str, Dict[str, Dict]]:
+    """Parses the sport radar timeline files in json format and extracts the event data.
+
+    This function provides access to the `Sport Event Timeline
+    <https://developer.sportradar.com/docs/read/handball/
+    Handball_v2#sport-event-timeline>`_ file from the dataprovider `Sportradar
+    <https://sportradar.com/>`_ exported in json format and returns Event objects for
+    all teams and segments of the game.
+
     Parameters
     ----------
-    filepath_events
+    filepath_events: str or pathlib.Path
+        Full path to json file where the Sport Event Timeline is saved.
 
     Returns
     -------
+    data_objects: Dict[str: Dict[str: Events]]
+        Dict with Events-objects for all teams and segments. For a usual league match
+        with two halves and two teams the order is:
 
+        {
+
+            1st_half: {team_a: Events, team_b: Events},
+
+            2nd_half: {team_a: Events, team_b: Events}
+
+        }
+
+    Notes
+    -----
+    Sportradar provides different information based on the respective Event type.
+    We decided to parse the first "layer" of information for each possible event type
+    that is listed in the `Handball v2 documentation FAQ
+    <https://developer.sportradar.com/docs/read/handball/
+    Handball_v2#frequently-asked-questions>`_ (most recent check: 11.01.2023). This
+    involves for example individual columns for the  home and away score at the event
+    type score_change but no individual columns for the players involved in Events, like
+    seven_m_awarded or shots, as they can contain different information based on the
+    situation. However, we parse the raw information as dict or list of dicts in the
+    respective column so the user can access it if necessary.
     """
 
-    with open(filepath_events) as f:
+    # load full json into memory
+    with open(str(filepath_events)) as f:
         events = json.load(f)
         f.close()
 
+    # check if timeline data exists
     if "timeline" not in events:
         raise ValueError("There appears to be no timeline data in this file.")
     else:
         timeline = events["timeline"]
+
+    # extract match id
     mID = events["sport_event"]["id"]
 
+    # create links from home/away to team id and team name
     teams = ["home", "away"]
 
     tID_link = {}
     home_away_link = {}
-    pID_link = {}
     for competitor in events["statistics"]["totals"]["competitors"]:
         tID_link.update({competitor["id"]: competitor["qualifier"]})
         home_away_link.update(
             {competitor["qualifier"]: (competitor["id"], competitor["name"])}
         )
-        for player in competitor["players"]:
-            pID_link.update({player["id"]: competitor["id"]})
 
+    # extract periods
+    periods = set(
+        [event["period_name"] for event in timeline if event["type"] == "period_start"]
+    )
+
+    # create team event dict
     columns = [
         "eID",
         "gameclock",
@@ -62,9 +105,6 @@ def read_sportradar_timeline(filepath_events):
         "players",
     ]
 
-    periods = set(
-        [event["period_name"] for event in timeline if event["type"] == "period_start"]
-    )
     # seems unnecessary
     segments = [f"HT{period}" for period in periods]
 
@@ -74,18 +114,22 @@ def read_sportradar_timeline(filepath_events):
     }
 
     period = None
+    # loop
     for event in timeline:
-        # get period
         if not period:
+            # get period
             if event["type"] == "period_start":
                 period = event["period_name"]
                 segment = f"HT{period}"
                 segment_start = datetime.datetime.fromisoformat(event["time"])
             else:
+                # skip events before first period starts
                 continue
 
+        # extract event, player and team ids and names
         eID = event["id"]
 
+        # add all teams as competitors if no competitor is specified in event
         competitor = [event["competitor"]] if "competitor" in event else teams
 
         tID = home_away_link[competitor[0]][0] if len(competitor) == 1 else None
@@ -95,7 +139,7 @@ def read_sportradar_timeline(filepath_events):
 
         event_name = event["type"]
 
-        # time
+        # extract time codes and matchclock
         time_stamp = datetime.datetime.fromisoformat(event["time"])
         time_delta = time_stamp - segment_start
         gameclock = time_delta.seconds
@@ -105,6 +149,7 @@ def read_sportradar_timeline(filepath_events):
         else:
             minute, second = (None, None)
 
+        # extract optional event information
         outcome = event["outcome"] if "outcome" in event else None
         home_score = event["home_score"] if "home_score" in event else None
         away_score = event["away_score"] if "away_score" in event else None
@@ -114,6 +159,7 @@ def read_sportradar_timeline(filepath_events):
         shot_type = event["shot_type"] if "shot_type" in event else None
         players = event["players"] if "players" in event else None
 
+        # add event to team event list
         for team in competitor:
             team_event_lists[team][segment]["eID"].append(eID)
             team_event_lists[team][segment]["gameclock"].append(gameclock)
@@ -135,6 +181,7 @@ def read_sportradar_timeline(filepath_events):
             team_event_lists[team][segment]["outcome"].append(outcome)
             team_event_lists[team][segment]["players"].append(players)
 
+        # flexible parser return for all segments and teams
         data_objects = {
             segment: {
                 team: Events(events=pd.DataFrame(data=team_event_lists[team][segment]))
