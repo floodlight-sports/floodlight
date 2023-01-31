e693fba927563cec7bd24288c677e15c46aa040a
hbiermann95 <58177509+hbiermann95@users.noreply.github.com>
2023-01-31 12:33:53 +0100

    feat!: integrate teamsheet logic to statsbomb parser  (#109)
    BREAKING CHANGE: replaces link creation functions with teamsheet parsing functions
    
    * feat: add teamsheet core object
    
    * docs: add definitions
    
    * docs: add teamsheet
    
    * docs: update events docstrings, integrate definitions
    
    * fix: broken test
    
    * test: add tests for teamsheet object
    
    * feat: add xIDs (#102)
    
    * test: add_xIDs function for create and overwrite
    
    * feat: add_xIDs functionality
    
    * refactor: docstrings and cleanup code
    
    * refactor: rename statsbomb parse function to exclude redundant use of statsbomb and align with other parsers
    
    * feat: read teamsheet from events and return as parser output
    
    * refactor: docstrings
    
    * refactor: update StatsBomb dataset class to new output shape
    
    * refactor: update matchsheet tutorial to new output shape
    
    * refactor: docstrings
    
    * fix: typo in naming of functions
    
    * test: add mock test with non_existent_file
    
    * refactor: add newline at end of file
    
    * refactor: docstrings
    
    * refactor: standardize variable names
    
    * feat: teamsheet as function argument support
    
    * feat: teamsheet as function argument support
    
    * refactor: docstrings
    
    * test: add test to get a single match from StatsBomb dataset
    
    * style: flake8
    
    * fix: convert pathlib to str
    
    * test: add test battery for StatssBomb dataset teamsheets
    
    * refactor: variable names and docstrings
    
    * refactor: variable names
    
    ---------
    
    Co-authored-by: draabe <draabx@posteo.org>

--- tests/test_io/test_datasets.py
+++ tests/test_io/test_datasets.py
@@ -2,6 +2,9 @@ import pytest
 import numpy as np
 
 from floodlight.io.datasets import EIGDDataset
+from floodlight.io.datasets import StatsBombOpenDataset
+from floodlight import Events
+from floodlight.core.teamsheet import Teamsheet
 
 
 # Test _transform staticmethod from EIGDDataset
@@ -15,3 +18,136 @@ def test_eigd_transform(
     assert np.array_equal(
         data_transformed, eigd_sample_data_floodlight_shape, equal_nan=True
     )
+
+
+# Test get method from StatsBombDataset
+@pytest.mark.unit
+def test_statsbomb_get() -> None:
+
+    dataset = StatsBombOpenDataset()
+    data = dataset.get(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+    )
+    assert isinstance(data[0], Events)
+    assert isinstance(data[1], Events)
+    assert isinstance(data[2], Events)
+    assert isinstance(data[3], Events)
+    assert isinstance(data[4], Teamsheet)
+    assert isinstance(data[5], Teamsheet)
+
+
+# Test get_teamsheet method from StatsBombDataset
+@pytest.mark.unit
+def test_statsbomb_get_teamsheet() -> None:
+
+    dataset = StatsBombOpenDataset()
+    teamsheets = dataset.get_teamsheets(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+    )
+    assert isinstance(teamsheets["Home"], Teamsheet)
+    assert isinstance(teamsheets["Away"], Teamsheet)
+    assert teamsheets["Home"].teamsheet.at[0, "team_name"] == "AC Milan"
+    assert teamsheets["Away"].teamsheet.at[0, "team_name"] == "Liverpool"
+    assert len(teamsheets["Home"].teamsheet) == 14
+    assert len(teamsheets["Away"].teamsheet) == 14
+    assert len(teamsheets["Home"].teamsheet.columns) == 6
+    assert len(teamsheets["Away"].teamsheet.columns) == 6
+
+
+# Test passing custom home_teamsheet to get method
+@pytest.mark.unit
+def test_statsbomb_get_pass_custom_home_teamsheet() -> None:
+
+    # get teamsheets
+    dataset = StatsBombOpenDataset()
+    teamsheets = dataset.get_teamsheets(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+    )
+
+    # customize home teamsheet
+    teamsheets["Home"].teamsheet.at[0, "player"] = "Dida"  # custom entry
+    teamsheets["Home"].teamsheet.at[0, "pID"] = 999999  # custom entry
+    teamsheets["Home"]["custom_col"] = 99  # custom column passed to function
+    teamsheets["Away"]["my_col"] = 99  # custom column but not passed to function
+
+    # call get function with custom teamsheet
+    data = dataset.get(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+        teamsheet_home=teamsheets["Home"],
+    )
+
+    assert data[4].teamsheet.at[0, "player"] == "Dida"
+    assert data[4].teamsheet.at[0, "pID"] == 999999
+    assert "custom_col" in data[4].teamsheet.columns
+    assert "my_col" not in data[5].teamsheet.columns
+
+
+# Test passing custom away_teamsheet to get method
+@pytest.mark.unit
+def test_statsbomb_get_pass_custom_away_teamsheet() -> None:
+    # get teamsheets
+    dataset = StatsBombOpenDataset()
+    teamsheets = dataset.get_teamsheets(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+    )
+
+    # customize home teamsheet
+    teamsheets["Home"].teamsheet.at[0, "player"] = "Dida"  # custom entry but not passed
+    teamsheets["Home"].teamsheet.at[0, "pID"] = 999999  # custom entry but not passed
+    teamsheets["Home"]["custom_col"] = 99  # custom column but not passed to function
+    teamsheets["Away"]["my_col"] = 99  # custom column passed to function
+
+    # call get function with custom teamsheet
+    data = dataset.get(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+        teamsheet_away=teamsheets["Away"],
+    )
+
+    assert data[4].teamsheet.at[0, "player"] != "Dida"
+    assert data[4].teamsheet.at[0, "pID"] != 999999
+    assert "custom_col" not in data[4].teamsheet.columns
+    assert "my_col" in data[5].teamsheet.columns
+
+
+# Test passing custom away_teamsheet to get method
+@pytest.mark.unit
+def test_statsbomb_get_pass_custom_teamsheets() -> None:
+    # get teamsheets
+    dataset = StatsBombOpenDataset()
+    teamsheets = dataset.get_teamsheets(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+    )
+
+    # customize home teamsheet
+    teamsheets["Home"].teamsheet.at[0, "player"] = "Dida"  # custom entry
+    teamsheets["Home"].teamsheet.at[0, "pID"] = 999999  # custom entry
+    teamsheets["Home"]["custom_col"] = 99  # custom column but not passed to function
+    teamsheets["Away"]["my_col"] = 99  # custom column passed to function
+
+    # call get function with custom teamsheet
+    data = dataset.get(
+        "Champions League",
+        "2004/2005",
+        "AC Milan vs. Liverpool",
+        teamsheet_home=teamsheets["Home"],
+        teamsheet_away=teamsheets["Away"],
+    )
+
+    assert data[4].teamsheet.at[0, "player"] == "Dida"
+    assert data[4].teamsheet.at[0, "pID"] == 999999
+    assert "custom_col" in data[4].teamsheet.columns
+    assert "my_col" in data[5].teamsheet.columns
